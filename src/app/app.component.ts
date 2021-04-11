0c9ac2569100563f54664eb1159d8902969c418e
Paolo Spadoni <paolo_spadoni@yahoo.it>
2021-04-11 19:19:32 +0200

    effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AppState } from './reducers';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
-import { logout } from './auth/auth.actions';
+import { login, logout } from './auth/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -14,58 +14,63 @@ import { logout } from './auth/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-    loading = true;
-    isLoggedIn$: Observable<Boolean>;
-    isLoggedOut$: Observable<Boolean>;
+  loading = true;
+  isLoggedIn$: Observable<Boolean>;
+  isLoggedOut$: Observable<Boolean>;
 
-    constructor(private router: Router, private store: Store<AppState>) {
+  constructor(private router: Router, private store: Store<AppState>) {
 
+  }
+
+  ngOnInit() {
+
+    const userProfile = localStorage.getItem('user');
+    if (userProfile) {
+      // if present in local storage, perform login
+      this.store.dispatch(login({ user: JSON.parse(userProfile) }));
     }
-
-    ngOnInit() {
-
-      this.router.events.subscribe(event  => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
-            break;
-          }
-
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
+    this.router.events.subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
         }
-      });
 
-      // this.store.subscribe(state => console.log('store value:', state));
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
 
-      // map converts the global state into a the specific type, -> boolean
-      // if there is a user profile, !!state['auth'].user is true
-      /*this.isLoggedIn$ = this.store.pipe(
-        map(state => !!state['auth'].user)
-      );*/
-      // if there is a user profile, !!state['auth'].user is false
-      /*this.isLoggedOut$ = this.store.pipe(
-        map(state => !state['auth'].user)
-      );*/
+    // this.store.subscribe(state => console.log('store value:', state));
 
-      this.isLoggedIn$ = this.store.pipe(
-        select(isLoggedIn)
-      );
+    // map converts the global state into a the specific type, -> boolean
+    // if there is a user profile, !!state['auth'].user is true
+    /*this.isLoggedIn$ = this.store.pipe(
+      map(state => !!state['auth'].user)
+    );*/
+    // if there is a user profile, !!state['auth'].user is false
+    /*this.isLoggedOut$ = this.store.pipe(
+      map(state => !state['auth'].user)
+    );*/
 
-      this.isLoggedOut$ = this.store.pipe(
-        select(isLoggedOut)
-      );
-    }
+    this.isLoggedIn$ = this.store.pipe(
+      select(isLoggedIn)
+    );
 
-    logout() {
-      this.store.dispatch(logout());
-    }
+    this.isLoggedOut$ = this.store.pipe(
+      select(isLoggedOut)
+    );
+  }
+
+  logout() {
+    this.store.dispatch(logout());
+  }
 
 }
---
+++ src/app/auth/auth.effects.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+import { AuthActions } from './action.types';
+
+@Injectable()
+export class AuthEffects {
+
+    constructor(private actions$: Actions, private router: Router) {
+    }
+
+    // we get notified when action login is triggered
+
+
+    // added  error handling and avoid manual subscription
+    // if something goes wrong the observable is recreated
+    // dispatch false is necessary in order to avoid loops. Otherwise happens:
+    // login action, listen to that, save to local storage, dispatch the same action,
+    // so it would create a loop
+    login$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(AuthActions.login),
+            tap(action => {
+                localStorage.setItem('user', JSON.stringify(action.user));
+            }
+            )
+        ),
+        { dispatch: false }
+    );
+
+    logout$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(AuthActions.logout),
+            tap(action => {
+                localStorage.removetItem('user');
+                this.router.navigateByUrl('/login');
+            }
+            )
+        ),
+        { dispatch: false }
+    );
+
+
+
+    // trivial syntaxs
+    /* actions$.subscribe(action => {
+         if (action.type === '[Login page] User Login') {
+             // access local storage
+             localStorage.setItem('user', JSON.stringify(action['user']));
+         }
+     });*/
+
+}
