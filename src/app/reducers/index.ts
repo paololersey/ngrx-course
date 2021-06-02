8200b77f5486b6da21f32c3357b6e0d9ccabb66d
Paolo Spadoni <paolo_spadoni@yahoo.it>
2021-06-02 08:12:48 +0200

    auth finished

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,6 +6,7 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
+import { Action } from 'rxjs/internal/scheduler/Action';
 import { environment } from '../../environments/environment';
 
 // tslint:disable-next-line:no-empty-interface
@@ -20,4 +21,13 @@ export const reducers: ActionReducerMap<AppState> = {
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
+
+// metareducer that logs
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before ' + state);
+    console.log('action ' + action);
+    return reducer(state, action);
+  };
+}
